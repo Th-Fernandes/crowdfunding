8058bf9bd910c94e501f6875d507702ffaa2ceb9
thiago fernandes <tjfctigo@gmail.com>
2021-07-19 16:31:43 -0300

    adicionando funcionalidades JS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// FUNCIONALIDADE DE ABRIR E FECHAR O MODAL
 const modalOpen = {
     open(){
         document.querySelector('.modal-overlay').classList.add('active');
@@ -7,3 +8,15 @@ const modalOpen = {
         document.querySelector('.modal-overlay').classList.remove('active');
     }
 }
+
+const payYourPrice = {
+    open(){
+        document.querySelector('.pledge').style.display = "flex";
+    },
+
+    close(){
+        document.querySelector('.pledge').style.display = "none";
+    },
+}
+
+    

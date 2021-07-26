1a0494881b5fd3ed655176ce5dadf829f1dd33dd
thiago fernandes <tjfctigo@gmail.com>
2021-07-26 16:39:45 -0300

    Tornando o modal de finalização funcional

--- index.js
+++ index.js
@@ -26,7 +26,23 @@ function verifyAmount() {
         alert("you selected a value below the selected pack. try again.");
         return false; // pra cancelar o envio do formulário
     } else {
-        return true;
+        true ;
+        
+        if(selectPrice >= 25) {
+            document.querySelector('#finishedBuy-modal').classList.add('active');
+        } //ESSA É A FUNCTION CONCLUDED
+
+        return selectPrice ;
+        
     }
 }
-    
+// FUNCIONALIDADE PARA ABRIR O MODAL DE AGRADECIMENTO, CASO O PREÇO MÍNIMO SEJA CUMPRIDO
+function concluded(selectPrice){
+    if(selectPrice >= 25) {
+        let searchModal = document.querySelector('#finishedBuy-modal').classList.add('active');
+    }
+}
+// FUNCIONALIDADE
+function conclusion() {
+    document.querySelector('#finishedBuy-modal').classList.remove('active');
+}
